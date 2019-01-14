af3e03d9842f01c9ea1f050040acad3474d522e9
Lulzim Fazlija <lf11661@seeu.edu.mk>
2019-01-14 17:30:31 +0200

    Updates

---
+++ precache-manifest.02431aa836ac1e33149ea6cc30d0bf43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/atop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f233ed4ed49de8fecbb0",
+    "url": "/atop/static/js/runtime~main.f233ed4e.js"
+  },
+  {
+    "revision": "3369ed1135fd52224d3a",
+    "url": "/atop/static/js/main.3369ed11.chunk.js"
+  },
+  {
+    "revision": "179484b90d9ddbd6d461",
+    "url": "/atop/static/js/1.179484b9.chunk.js"
+  },
+  {
+    "revision": "3369ed1135fd52224d3a",
+    "url": "/atop/static/css/main.85a9719a.chunk.css"
+  },
+  {
+    "revision": "eb1273ca77a7a8c51d7927fd4e86a472",
+    "url": "/atop/index.html"
+  }
+];
